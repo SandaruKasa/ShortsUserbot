25946a659aad4ebd50a97ef16c53c39dd5590ffc
SandaruKasa <sandarukasa@ya.ru>
2023-08-28 20:56:34 +0300

    Flake-parts

--- flake.nix
+++ flake.nix
@@ -1,62 +1,47 @@
 {
   description = "A Telegram userbot that converts all YouTube Shorts link to normal videos";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-  };
-
-  outputs = { self, nixpkgs }:
-    let
-      allSystems = [
+  outputs = inputs @ { nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
         "x86_64-linux"
         "aarch64-linux"
       ];
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
 
+      perSystem = { pkgs, ... }:
+        let
+          takeAttrs = attrList: x: (map (attr: x."${attr}") attrList);
 
-      takeAttrs = attrList: x: (map (attr: x."${attr}") attrList);
+          python = pkgs.python311;
+          pythonPkgs = python.pkgs;
+          pythonDeps = [
+            "pyrogram"
+            "tgcrypto"
+          ];
+          pythonDevDeps = [
+            "black"
+            "mypy"
+            "isort"
+          ];
+        in
+        {
+          packages.default =
+            pythonPkgs.buildPythonApplication {
+              name = "shorts-userbot";
+              format = "pyproject";
+              buildInputs = with pythonPkgs; [ setuptools ];
+              propagatedBuildInputs = takeAttrs pythonDeps pythonPkgs;
+              src = ./.;
+            };
 
-      selectPython = pkgs: pkgs.python311;
-      pythonDeps = [
-        "pyrogram"
-        "tgcrypto"
-      ];
-      pythonDevDeps = [
-        "black"
-        "mypy"
-        "isort"
-      ];
-    in
-    {
-      packages = forAllSystems ({ pkgs }: {
-        default =
-          let
-            python = selectPython pkgs;
-            pythonPkgs = python.pkgs;
-          in
-          pythonPkgs.buildPythonApplication {
-            name = "shorts-userbot";
-            format = "pyproject";
-            buildInputs = with pythonPkgs; [ setuptools ];
-            propagatedBuildInputs = takeAttrs pythonDeps pythonPkgs;
-            src = ./.;
-          };
-      });
-
-      devShells = forAllSystems ({ pkgs }: {
-        default =
-          let
-            python = (selectPython pkgs).withPackages
-              (takeAttrs (pythonDeps ++ pythonDevDeps));
-          in
-          pkgs.mkShellNoCC {
-            packages = [
-              python
-              pkgs.nixpkgs-fmt
-            ];
-          };
-      });
+          devShells.default =
+            pkgs.mkShellNoCC {
+              packages = [
+                (python.withPackages (takeAttrs (pythonDeps ++ pythonDevDeps)))
+                pkgs.nixpkgs-fmt
+              ];
+            };
+        };
     };
 }
+    
